e27e203e78dd2f58f911c55cab9c96a27f9f5102
kladaFOX <klapatok1980@gmail.com>
2019-10-23 13:56:34 +0300

    add scopes

--- app/models/subject_mark.rb
+++ app/models/subject_mark.rb
@@ -1,4 +1,26 @@
 class SubjectMark < ApplicationRecord
   belongs_to :student
   belongs_to :subject
+
+  def self.unsatisfactory
+    SubjectMark.where("mark <= 3").includes(:student)
+  end
+
+  def self.satisfactory
+    SubjectMark.where("mark >= 4").includes(:student)
+  end
+
+  def self.average
+    SubjectMark.group(:student_id).select(:student_id).average(:mark)
+  end
+  def self.last_names_average
+    output = {}
+    SubjectMark.average.each do |key, value|
+      last_name = Student.find(key).last_name
+      output[last_name] = value.to_f
+    end
+    output
+  end
+
+
 end
--- queries.rb
+++ queries.rb
@@ -1,13 +1,15 @@
 puts 'find_by:'
-Student.find_by first_name: 'Ilya'
+Student.find_by(first_name: 'Ilya')
 puts 'sort:'
-Student.order :last_name
+Student.order(:last_name)
 Student.find_each { |x| puts x.last_name }
 puts 'all kind of marks:'
 puts SubjectMark.select(:mark).group(:mark).count
-puts 'students with 2 or lower points:'
-dvoe = SubjectMark.where("mark <= 2").includes(:student)
+puts 'students with 3 or lower points:'
+dvoe = SubjectMark.unsatisfactory
 dvoe.each {|x| puts x.student.last_name}
-puts 'students with 3 or higher points:'
-dvoe = SubjectMark.where("mark >= 3").includes(:student)
+puts 'students with 4 or higher points:'
+dvoe = SubjectMark.satisfactory
 dvoe.each {|x| puts x.student.last_name}
+puts 'average marks:'
+puts SubjectMark.last_names_average
