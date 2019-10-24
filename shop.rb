2eee7cddbea2789ea23d255e6ccf5e1338fe67c9
kladaFOX <klapatok1980@gmail.com>
2019-10-24 18:32:38 +0300

    query to shop

--- shop.rb
+++ shop.rb
@@ -3,3 +3,5 @@ c1 = Course.create(name: "Ruby", price: 100)
 c2 = Course.create(name: "Python", price: 150)
 Position.create(cart_id: cart.id, course_id: c1.id, quantity: 2)
 Position.create(cart_id: cart.id, course_id: c2.id, quantity: 1)
+puts("У студента: #{Student.last.first_name} в корзине лежат:")
+Student.last.cart.courses.each { |x| puts("name: #{x.name}, price: #{x.price}")}
