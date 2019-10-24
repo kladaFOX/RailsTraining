d820d085cd7e8ab921dd5e4d932f637357a9c65c
kladaFOX <klapatok1980@gmail.com>
2019-10-24 18:02:08 +0300

    add shop

---
+++ shop.rb
@@ -0,0 +1,5 @@
+cart = Cart.create(student_id: Student.last.id)
+c1 = Course.create(name: "Ruby", price: 100)
+c2 = Course.create(name: "Python", price: 150)
+Position.create(cart_id: cart.id, course_id: c1.id, quantity: 2)
+Position.create(cart_id: cart.id, course_id: c2.id, quantity: 1)
